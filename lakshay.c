b56aa6fc142875b6e8d48d42274c6326a3b98cde
lakshaydaver <31953307+lakshaydaver@users.noreply.github.com>
2017-09-27 21:55:46 +0530

    Update lakshay.c

--- lakshay.c
+++ lakshay.c
@@ -1,5 +1,29 @@
 #include<stdio.h>
 void main()
 {
-printf("heelo in git hub");
+
+char c; 
+int x=3,y=2,z;
+int a=1101,b=10101,u,k;
+printf("\ntype the value of ascii charcter \n");
+
+scanf("%c",&c);
+printf("\n print the value of ascii value of a:%d\n",c);
+
+
+z=x<y? x:y;
+printf("the value of z is:%d\n",z);
+
+u=a^b;
+printf(" \n the xor value is :\n%d\n",u);
+
+
+k=~a;
+printf("\n the compliment is :%d",k);
+
+
+
+
+
+
 }
